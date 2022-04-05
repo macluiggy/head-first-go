ceaff248ec10dad1073eec6aa0347bb59a4ef268
luiggy macias <ferrinluiggy@gmail.com>
2022-04-05 15:26:01 -0500

    scopte

--- chapter 2/pass_fail.go
+++ chapter 2/pass_fail.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // "fmt"
@@ -15,10 +17,19 @@ func main()  {
 	fmt.Print("Enter your grade: ")
 	reader := bufio.NewReader(os.Stdin) // newreader es para que espere por input, y stdin es de donde se lee en este caso el teclado
 	input, err := reader.ReadString('\n') // lee hasta que encuentra un salto de linea
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64) // convierte el string a float64
 	if err != nil {
 		log.Fatal(err)
 	}
-	println(input)
+	var status string
+	if grade >= 60 {
+		status = "passed"
+	} else {
+		status = "failed"
+	}
+	fmt.Println("A grado of", grade, "is", status)
+	// println(input)
 	// fmt.Println("!false")
 
 }
