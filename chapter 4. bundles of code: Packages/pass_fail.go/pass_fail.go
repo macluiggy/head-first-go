bcdf2b57a0d1881feeee14cc70bfe237be697209
luiggy macias <ferrinluiggy@gmail.com>
2022-06-02 13:51:37 -0500

    dsdsds

---
+++ chapter 4. bundles of code: Packages/pass_fail.go/pass_fail.go
@@ -0,0 +1,32 @@
+// reporta si un estudiante ha aprobado o no dependiendo de su nota
+package main
+
+import (
+	"fmt"
+	"keyboard"
+	"log"
+)
+
+// "fmt"
+
+func main() {
+	fmt.Print("Enter your grade: ")
+	// reader := bufio.NewReader(os.Stdin) // newreader es para que espere por input, y stdin es de donde se lee en este caso el teclado
+	// input, err := reader.ReadString('\n') // lee hasta que encuentra un salto de linea
+	// input = strings.TrimSpace(input)
+	// grade, err := strconv.ParseFloat(input, 64) // convierte el string a float64
+	grade, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string
+	if grade >= 60 {
+		status = "passed"
+	} else {
+		status = "failed"
+	}
+	fmt.Println("A grade of", grade, "is", status)
+	// println(input)
+	// fmt.Println("!false")
+
+}
